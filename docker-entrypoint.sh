62d2942f846a645ffd624a42dd549148aa6872c7
georgekav2 <>
2023-08-18 02:06:44 +0200

    Self update ytb-dl on each container restart

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,9 @@ umask ${UMASK}
 echo "Creating download directory (${DOWNLOAD_DIR}), state directory (${STATE_DIR}), and temp dir (${TEMP_DIR})"
 mkdir -p "${DOWNLOAD_DIR}" "${STATE_DIR}" "${TEMP_DIR}"
 
+echo "Updating ytp-dl"
+pip install --upgrade yt-dlp
+
 if [ `id -u` -eq 0 ] && [ `id -g` -eq 0 ]; then
     if [ "${UID}" -eq 0 ]; then
         echo "Warning: it is not recommended to run as root user, please check your setting of the UID environment variable"
